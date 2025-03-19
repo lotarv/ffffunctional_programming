2dfb3118921320ed88a6e958b5e238b005ee966d
lotarv <lotarev200445@gmail.com>
2025-03-19 16:48:35 +0300

    task5: sum of digits recursion DOWN

--- lab5/Program.fs
+++ lab5/Program.fs
@@ -22,6 +22,16 @@ let rec digitSumUp num =
     if num = 0 then 0
     else (num % 10) + (digitSumUp(num / 10))
 
+
+let digitSumDown num = 
+    let rec digitSumTail num acc = 
+        if num = 0 then acc
+        else
+            let digit = num % 10
+            let cur_num = num / 10
+            digitSumTail cur_num (acc + digit)
+    digitSumTail num 0
+
 [<EntryPoint>]
 let main (argv :string[]) = 
     //task 1
@@ -56,6 +66,10 @@ let main (argv :string[]) =
     let num = Console.ReadLine() |> int
     let sum = digitSumUp num
     printfn "Digit sum of %d is: %d" num sum
-
-
+       
+    Console.WriteLine("task5: Digit sum (Recursion DOWN)")
+    Console.WriteLine("Enter number:")
+    let num = Console.ReadLine() |> int
+    let sum = digitSumDown num
+    printfn "Digit sum of %d is: %d" num sum
     0
