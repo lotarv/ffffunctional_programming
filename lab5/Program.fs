e7fef648ddd41aaab81468d5910595c62183e7bd
lotarv <lotarev200445@gmail.com>
2025-03-19 15:13:54 +0300

    task2 completed

--- lab5/Program.fs
+++ lab5/Program.fs
@@ -1,7 +1,24 @@
 ﻿open System
 
+type SolveResult =
+    None | Linear of float | Quadratic of float*float
+
+let solve a b c =
+    let D = b*b - 4.0*a*c
+    if a=0.0 then
+        if b=0. then None
+        else Linear(-c/b)
+    else
+        if D<0. then None
+        else Quadratic(((-b+sqrt(D))/(2.0*a), (-b-sqrt(D))/(2.0*a)))
 [<EntryPoint>]
 let main (argv :string[]) = 
     //task 1
-    printfn "Hello World"
+    Console.WriteLine("task1: Hello world")
+    Console.WriteLine("task2: Solving quadratic quation")
+    let res = solve 1.0 2.0 -3.0
+    match res with
+     | None -> Console.WriteLine("No solution")
+     | Linear(x) -> printfn "Linear quation x = %f" x
+     | Quadratic(x1,x2) -> printfn "Quadratic equation: x1=%f, x=%f" x1 x2
     0
